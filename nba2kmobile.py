fe98b2cf3a50d653d1d3ce940310e06aaf4840f8
ohihc5 <177495282+ohihc5@users.noreply.github.com>
2026-02-16 20:34:39 +0000

    chore: Update NBA2K selectors (automated)

--- nba2kmobile.py
+++ nba2kmobile.py
@@ -14,21 +14,21 @@ PLAYER_IDS = [
 
 # --- AUTO-GENERATED SELECTORS START ---
 CLAIM_SELECTORS = [
-    "button#buy-button-1z6wB0wQsJaZtoQy08tymh",  # D1 CLAIM
-    "button#buy-button-7a4WPK7dWoZfbFk0Gc0Qcg",  # D2 CLAIM
-    "button#buy-button-4GSIvwseVaqB2CgmdXUL3J",  # D3 CLAIM
-    "button#buy-button-2aZYWi9M09LmEAx4qlHLV3",  # D4 CLAIM
-    "button#buy-button-4Tvqy3Eaa94uyh35dXf5Ly",  # D5 CLAIM
-    "button#buy-button-9zjPZZ6iEr759q5x5jHzI",  # D6 CLAIM
-    "button#buy-button-1Zgk8LZnMNUNhoqxAOMEHN",  # D7 CLAIM
-    "button#buy-button-6qScXBUkNuwG2vmtlufNPm",  # Final Reward CLAIM
+    "button#buy-button-4CEd0sZsJQkJLHHK3MfN1X",  # D1 CLAIM
+    "button#buy-button-7qEnYljHbJv3wr2XUwqRGM",  # D2 CLAIM
+    "button#buy-button-4LjOBw6cLYBOQeQirrGReu",  # D3 CLAIM
+    "button#buy-button-1GJ4YRNcSsvSW3LhTRVSek",  # D4 CLAIM
+    "button#buy-button-6yoQsiIjcpFHzkcuGem8Cq",  # D5 CLAIM
+    "button#buy-button-4Gl8WHgKaky1spdD9uwZBK",  # D6 CLAIM
+    "button#buy-button-4NPITKKINyn9Su97NkdOQ9",  # D7 CLAIM
+    "button#buy-button-2BE3d3BBI1Aj8SNJ3kl5N1",  # Final Reward CLAIM
 ]
 
 # D7 CLAIM selector
-D7_SELECTOR = "button#buy-button-1Zgk8LZnMNUNhoqxAOMEHN"
+D7_SELECTOR = "button#buy-button-4NPITKKINyn9Su97NkdOQ9"
 
 # Final Reward CLAIM selector
-FINAL_REWARD_SELECTOR = "button#buy-button-6qScXBUkNuwG2vmtlufNPm"
+FINAL_REWARD_SELECTOR = "button#buy-button-2BE3d3BBI1Aj8SNJ3kl5N1"
 # --- AUTO-GENERATED SELECTORS END ---
 def create_driver():
     options = webdriver.ChromeOptions()
