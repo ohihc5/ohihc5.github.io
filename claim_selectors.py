bcc9be78b6d59c3a7f4bf7e0c94e25811fc768ff
ohihc5 <177495282+ohihc5@users.noreply.github.com>
2026-06-15 22:20:29 +0000

    chore: Update NBA2K selectors (automated)

--- claim_selectors.py
+++ claim_selectors.py
@@ -1,18 +1,18 @@
 # Auto-generated CLAIM_SELECTORS from button scanner
 
 CLAIM_SELECTORS = [
-    "button#buy-button-1foPAy8OhUVZnYHeb8TgF3",  # D1 CLAIM
-    "button#buy-button-1fz2uKEPldZRIyHnBVJlxg",  # D2 CLAIM
-    "button#buy-button-3Vkk13fm9yKQYjSDmQCjmH",  # D3 CLAIM
-    "button#buy-button-7HhKyPaLv6pDFdzWYTjnlR",  # D4 CLAIM
-    "button#buy-button-7xr26ZeX4i4AilA76faQIn",  # D5 CLAIM
-    "button#buy-button-6gBu7Lg7uAUK9m31nJuatM",  # D6 CLAIM
-    "button#buy-button-5YU0T8T32k3G1TlxFuno0A",  # D7 CLAIM
-    "button#buy-button-3X8ipK7511nauGq038FOdP",  # Final Reward CLAIM
+    "button#buy-button-1zokIWy48Y6g0daTc15qY4",  # D1 CLAIM
+    "button#buy-button-46G7HXESBpd11e9yhMQFW9",  # D2 CLAIM
+    "button#buy-button-OiSEpQdKoWWfUupn0VYAj",  # D3 CLAIM
+    "button#buy-button-1vdybDz48XbdHQHvX2wo5A",  # D4 CLAIM
+    "button#buy-button-7hQmr4DS9bmK95oz5gGmhv",  # D5 CLAIM
+    "button#buy-button-7eGTQCPAXJEgAB3ucguj0z",  # D6 CLAIM
+    "button#buy-button-74MzIPcIKjvrha7LI9vMsi",  # D7 CLAIM
+    "button#buy-button-1OioXsk0WFID7GwZRFVNOy",  # Final Reward CLAIM
 ]
 
 # D7 CLAIM selector
-D7_SELECTOR = "button#buy-button-5YU0T8T32k3G1TlxFuno0A"
+D7_SELECTOR = "button#buy-button-74MzIPcIKjvrha7LI9vMsi"
 
 # Final Reward CLAIM selector
-FINAL_REWARD_SELECTOR = "button#buy-button-3X8ipK7511nauGq038FOdP"
+FINAL_REWARD_SELECTOR = "button#buy-button-1OioXsk0WFID7GwZRFVNOy"
